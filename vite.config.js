77876859d0cc69d71e6d9841860f56c58bf31229
ArkadiuszSzwarocki <dacklowicz@wp.pl>
2026-01-21 22:58:27 +0100

    21.01.2026 22:45:00 +0100

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ export default {
   plugins: [react()],
   server: {
     allowedHosts: ['filipinka.myqnapcloud.com', 'mlecznadroga.mycloudnas.com'],
-    port: 5178,
+    port: 5173, // Zmieniono z 5178 na 5173, aby pasowało do logów
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:3000/',
+        target: 'http://127.0.0.1:8089/', // Zmieniono z 3000 na 8089
         changeOrigin: true,
       }
     }
