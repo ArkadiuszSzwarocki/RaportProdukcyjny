2b3e55e98916bc40bad53b996478221ce2681786
ArkadiuszSzwarocki <dacklowicz@wp.pl>
2026-01-21 22:54:50 +0100

    21.01.202622:45:12 +0100

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Umożliwia dostęp z sieci (np. z QNAP)
-    port: 5173,      // Port frontendu
+ server: {
+    allowedHosts: ['filipinka.myqnapcloud.com', 'mlecznadroga.mycloudnas.com'],
+    port: 5173, // Zmieniono z 5178 na 5173, aby pasowało do logów
     proxy: {
-        '/api': {
-        target: 'http://localhost:8089', // Przekierowanie do backend API (dev proxy -> backend na 8089)
+      '/api': {
+        target: 'http://127.0.0.1:8089/', // Zmieniono z 3000 na 8089
         changeOrigin: true,
-        secure: false,
-      },
-    },
+      }
+    }
   },
 });
