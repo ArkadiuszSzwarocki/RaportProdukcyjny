3f3e54a8453db8a7295cdbaf60428ef13dfcf2ca
ArkadiuszSzwarocki <dacklowicz@wp.pl>
2026-07-17 22:01:07 +0200

    chore: Usunięcie z repozytorium starych backupów, skryptów testowych i plików roboczych

--- db-init/02-drop-szarze_parametry.sql
+++
@@ -1,4 +0,0 @@
--- Skrypt do usunięcia tabeli szarze_parametry
--- Uruchom ostrożnie na środowisku, upewnij się, że masz backup bazy.
-
-DROP TABLE IF EXISTS szarze_parametry;
--- scratch/check_processes.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-try:
-    cmd = 'powershell -Command "Get-CimInstance Win32_Process -Filter \\"Name = \'python.exe\'\\" | Select-Object ProcessId, CommandLine | Format-Table -Wrap"'
-    out = subprocess.check_output(cmd, shell=True)
-    print(out.decode('utf-8', errors='ignore'))
-except Exception as e:
-    print("Error:", e)
